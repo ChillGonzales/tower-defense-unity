8afb8aa193fd79c99be3a7c3ec968fc32b2a74b4
Colin Monroe <colin.monroe@gentex.com>
2017-11-30 20:42:14 -0500

    Done with #5

---
+++ tower-defense-unity/Assets/Scripts/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    public float speed = 70f;
+    public GameObject impactEffect;
+
+    private Transform target;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+
+    private void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 direction = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        // Length of direction vector is the distance to the target
+        // So if our distance to move is less than that magnitude we are going to hit the target
+        if (direction.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+    }
+
+    private void HitTarget()
+    {
+        var effect = Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effect, 2f);
+        Destroy(gameObject);
+        Destroy(target.gameObject);
+    }
+}
---
+++ tower-defense-unity/Assets/Scripts/Turret.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+    [Header("Attributes")]
+    public float range = 15f;
+    public float turnSpeed = 10f;
+    public float fireRate = 1f;
+
+    [Header("GameObject References")]
+    public Transform partToRotate;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    private Transform target;
+    private float fireCountdown = 0f;
+
+	private void Start ()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+	}
+
+    private void UpdateTarget()
+    {
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (var enemy in WaveSpawner.Enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if ((distanceToEnemy < shortestDistance) && (distanceToEnemy <= range))
+            {
+                nearestEnemy = enemy;
+                shortestDistance = distanceToEnemy;
+            }
+        }
+
+        target = nearestEnemy != null ? nearestEnemy.transform : null;
+    }
+
+	private void Update ()
+    {
+        if (target == null)
+            return;
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles; // smooths the rotation
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if (fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+	}
+
+    private void Shoot()
+    {
+        var bulletObj = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletObj.GetComponent<Bullet>();
+        if (bullet != null)
+            bullet.Seek(target);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(gameObject.transform.position, range);
+    }
+}
