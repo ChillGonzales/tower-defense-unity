8c231771416a57d2dabbb6bb94ad38ae6704a487
Colin Monroe <cmmonroe2010@gmail.com>
2017-12-11 18:48:36 -0500

    done with #7

---
+++ tower-defense-unity/Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
+
+	void Update ()
+    {
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("e"))
+        {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey("q"))
+        {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.Self);
+        }
+	}
+}
